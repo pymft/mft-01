5fc63f8053706a67ed476d3bc19ffeaed56a3133
Vahid Naeini <naeini.v@gmail.com>
2019-01-25 13:09:23 +0330

    dictionaries

---
+++ S01/key_value_objects.py
@@ -0,0 +1,11 @@
+lst = [1, 2, 3, 4, 5]
+val = lst[1]
+print(val)
+
+person_list = ['Smith', 'John',  31]
+person_dict = {'last name': 'Smith', 'first name': 'John', 'age': 31}
+
+val_1 = person_list[0]
+print(val_1)
+val_2 = person_dict['first name']
+print(val_2)
--- S01/list_and_tuple_operators.py
+++ S01/list_and_tuple_operators.py
@@ -9,3 +9,8 @@ print(lst4)
 
 lst5 = lst1 * 3
 print(lst5)
+
+# in
+lst_new = [1, 2, 3, 5, 7, 11, 13]
+cond = 0 in lst_new
+print(cond)
