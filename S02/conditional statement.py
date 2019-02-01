759ecebaa2b6a2b7b566a7c5455a20837bc0312f
Vahid Naeini <naeini.v@gmail.com>
2019-02-01 13:44:16 +0330

    conditional statements

---
+++ S02/conditional statement.py
@@ -0,0 +1,15 @@
+num = input("please enter a number : ")
+num = int(num)
+
+cond1 = num > 10
+cond2 = num == 100
+cond3 = num % 2 == 0
+
+
+print(cond1, cond2, cond3)
+
+
+if num > 100:
+    print("hello 1")
+else:
+    print("das vi daniya")
