f2e005894c8c6d449272fad3f5a637fdc0c816d3
Vahid Naeini <naeini.v@gmail.com>
2019-02-08 11:49:24 +0330

    loops

--- S03/assignment2.py
+++ S03/assignment2.py
@@ -1,12 +1,25 @@
-#       a            b           c               d
+#       a=0          b=10        c=20            d=100
 #       |            |           |               |
 #       v            v           v               v
-#  <----+============+-----------+===============+------>
+#  <----+############+-----------+###############+------>
 
 a = 0
 b = 10
 c = 20
 d = 100
 
-num = int(input("num = "))
+# num = int(input("num = "))
 
+# if (num > a and num < b) or (num > c and num < d):
+#     print("is in the range!")
+# else:
+#     print("is not in the range!")
+
+num = 5
+if (num > a and num < d):
+    if not (num > b and num < c):
+        print("is in the range!")
+    else:
+        print("the number is the range of b to c ")
+else:
+    print("is not in the range!")
---
+++ S03/loops/while_loops.py
@@ -0,0 +1,13 @@
+i = 0
+limit = 100
+
+sum_of_numbers = 0
+
+while i <= limit:
+    sum_of_numbers = sum_of_numbers + i
+    i = i + 1
+
+print(sum_of_numbers)
+
+
+
