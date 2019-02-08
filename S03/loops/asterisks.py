2f2d2ce010ae4927842e2ea6a9fe4e38b199c396
Vahid Naeini <naeini.v@gmail.com>
2019-02-08 13:20:39 +0330

    loops - continue

--- S03/loops/asterisks.py
+++ S03/loops/asterisks.py
@@ -1,4 +1,6 @@
 num = 6
+pattern = "*"
+pattern_whitespace = " "
 
 # while
 # *
@@ -7,6 +9,12 @@ num = 6
 # ****
 # *****
 # ******
+i = 0
+while i < num:
+    i += 1
+    line = pattern * i
+    print(line)
+
 
 # while + str methods
 #       *
@@ -15,6 +23,12 @@ num = 6
 #    *******
 #   *********
 #  ***********
+i = 0
+while i < num:
+    i += 1
+    line = "*" * (2*i-1)
+    line_centered = line.center(2*num - 1)
+    print(line_centered)
 
 # if
 #       *
@@ -24,6 +38,35 @@ num = 6
 #   *       *
 #  *         *
 
+i = 0
+while i < num:
+    i += 1
+    if i == 1:
+        print(pattern.center(2*num - 1))
+    else:
+        line = " " * (2*i-3)
+        line = "*" + line + "*"
+        line_centered = line.center(2*num - 1)
+        print(line_centered)
+
+
+
+
+
+
+
+
+
+first_line = "*".center(2*num - 1)
+print(first_line)
+i = 1
+while i < num:
+    i += 1
+    line = " " * (2*i-3)
+    line = "*" + line + "*"
+    line_centered = line.center(2*num - 1)
+    print(line_centered)
+
 # if
 #       *
 #      * *
@@ -31,3 +74,15 @@ num = 6
 #    *     *
 #   *       *
 #  ***********
+
+i = 0
+while i < num:
+    i += 1
+    if i == 1 or i == num:
+        line = pattern * (2*i-1)
+        print(line.center(2*num - 1))
+    else:
+        line = " " * (2*i-3)
+        line = "*" + line + "*"
+        line_centered = line.center(2*num - 1)
+        print(line_centered)
