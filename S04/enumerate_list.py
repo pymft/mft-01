cff32802326d2a6989bdab858aba7e5658cfcdb1
Vahid Naeini <naeini.v@gmail.com>
2019-02-15 11:50:43 +0330

    test

--- S04/enumerate_list.py
+++ S04/enumerate_list.py
@@ -1,10 +1,12 @@
 lst = [1, 32, 12, 331, 90]
 
-#
-# 
-# 
-
 # enumerated_lst = [(0, 1), (1, 32), (2, 12), (3, 331), (4, 90)]
+enumerated_lst = []
+for i in range(len(lst)):
+    val = (i, lst[i])
+    enumerated_lst.append(val)
+
+print(enumerated_lst)
 
 for i, val in enumerate(lst):
     print(i, val)
--- S04/zip_two_lists.py
+++ S04/zip_two_lists.py
@@ -1,12 +1,17 @@
-lst_one = [10, 20, 30, 40]
-lst_two = [-1, -2, -3, -4]
+lst_one = [10, 20, 30, 40, 10, 20, 30, 40, 10, 20, 30, 40, 10, 20, 30, 40, 10, 20, 30, 40, 10, 20, 30, 40, 10, 20, 30, 40, ]
+lst_two = [-1, -2, -3, -4, -1, -2, -3, -4, -1, -2, -3, -4, -1, -2, -3, -4, -1, -2, -3, -4, -1, -2, -3, -4, -1, -2, -3, -4, ]
 
+members = len(lst_one)
 
-#
-# Add code here !!! 
-#
+lst_zip = []
+for i in range(members):
+    a = lst_one[i]
+    b = lst_two[i]
+    val = (a, b)
+    lst_zip.append(val)
 
-lst_zip = [(10, -1), (20, -2), (30, -3), (40, -4)]
+print(lst_zip)
+# lst_zip = [(10, -1), (20, -2), (30, -3), (40, -4)]
 
 # if len(lst_one) == len(lst_two)
 # for i in range(len(lst_one)):
@@ -14,6 +19,10 @@ lst_zip = [(10, -1), (20, -2), (30, -3), (40, -4)]
 #     b = lst_two[i]
 #     diff = a - b
 #     print(i, diff)
+
+
+for a, b in lst_zip:
+    print(a-b)
 
 for a, b in zip(lst_one, lst_two):
     print(a-b)
