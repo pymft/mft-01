7c9f36d1b2c609fa5368e9e91e2e73778fd96bda
Vahid Naeini <naeini.v@gmail.com>
2019-02-15 09:46:42 +0330

    review and assignments

---
+++ S04/assignment.py
@@ -0,0 +1,17 @@
+num = int(input('enter a number : '))
+
+# 1|num num|num
+# for  in :
+#     if .... : # check
+#         #
+#
+
+i = 1
+while :
+    i = i + 1     # i += 1
+    if
+
+i = 1
+while :
+    i = i + 1  # i += 1
+    if
---
+++ S04/reciew.py
@@ -0,0 +1,25 @@
+# num = 10
+#
+# if num >= 5:
+#     print(num)
+#     print("number is greater than or equal to 5")
+# elif num >= 0:
+#     print("number is in the range of 0 to 5")
+# elif num >= -10:
+#     print("number is in the range of -10  to 0")
+
+# i = 1
+# while i < 10:
+#     print(i)
+#     i = i + 1
+#
+# print("end of the loop!")
+
+a = int(input('start : '))
+b = int(input('stop : '))
+# lst = [1, 2, 3, 4, 5, 6]
+
+for var in range(a, b+1):
+    print(var)
+
+
