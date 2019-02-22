3ecec2ca6f6052dc0d228fdb9a208b3555afa09f
Vahid Naeini <naeini.v@gmail.com>
2019-02-22 10:28:31 +0330

    read files part 2 (read two separate vars from an input file)

--- S05/files_and_with_block/write_into_files.py
+++ S05/files_and_with_block/write_into_files.py
@@ -1,11 +1,16 @@
 f1 = open('input.txt')
 text = f1.read()
-n = int(text)
-print(n)
+lines = text.split('\n')  # ['  30  ', '50']
+m_str = lines[0].strip()
+n_str = lines[1].strip()
+m = int(m_str)
+n = int(n_str)
+
+# n = int(lines[0].strip())
 f1.close()
 
 f = open('./output.txt', 'w')
-for i in range(1, n+1):
+for i in range(m, n+1):
     line = "*" * (i)
     line = line + '\n'
     f.write(line)
