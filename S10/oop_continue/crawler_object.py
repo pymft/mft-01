aab98d230a982e04737b6bef799e9ecce53e542c
iamvee <naeini.v@gmail.com>
2019-04-19 10:46:42 +0330

    first draft of browser, and using Singleton pattern

--- S10/oop_continue/crawler_object.py
+++ S10/oop_continue/crawler_object.py
@@ -3,9 +3,17 @@ import urllib.request
 
 
 class Browser:
+    _instances = {}
+
+    def __new__(cls, *args, **kwargs):
+        if args[0] in cls._instances.keys():
+            return Browser._instances[args[0]]
+        return super().__new__(cls)
+
     def __init__(self, url):
         self.url = url
         self.__data = None
+        Browser._instances.update({self.url: self})
 
     @property
     def data(self):
@@ -15,12 +23,28 @@ class Browser:
 
         return self.__data
 
+    def links(self):
+        pass
+
 
 b = Browser("https://python.org")
 c = Browser("https://python.org")
-times = []
-times.append(time())
-text = b.data
-times.append(time())
-print(times[-1] - times[-2])
-print(len(text))
+
+
+b.links[5].data
+
+# print(id(b))
+# print(id(c))
+#
+# times = []
+# times.append(time())
+# text = b.data
+# times.append(time())
+# print(times[-1] - times[-2])
+# print(len(text))
+#
+# times.append(time())
+# text = c.data
+# times.append(time())
+# print(times[-1] - times[-2])
+# print(len(text))
